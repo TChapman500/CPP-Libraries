456c942eef90c0b3fd062e8bc68c4662098c84ae
Timothy Chapman <tchapman500@zohomail.com>
2021-05-09 06:23:11 -0400

    Updated Event system to be header only.

--- CrossPlatform/Event/Event.h
+++ CrossPlatform/Event/Event.h
@@ -3,28 +3,67 @@
 
 namespace TChapman500
 {
-	class EventListener
-	{
-	public:
-		virtual ~EventListener() {}
-	};
-
-	typedef void (EventListener:: *Function)(void *);
+	class EventSubscriber { };
+	typedef void (EventSubscriber:: *EventFunction)(void *);
 
 	class Event
 	{
 	public:
-		Event();
-		~Event();
+		Event() { }
 
-		void AddListener(EventListener *listener, Function function);
-		void RemoveListener(EventListener *listener, Function function);
+		void Subscribe(EventSubscriber *subscriber, EventFunction function)
+		{
+			// Search for subscriber and function in list.
+			size_t subscribers = SubscriberList.size();
+			for (size_t i = 0; i < subscribers; i++)
+			{
+				if (SubscriberList[i].first == subscriber && SubscriberList[i].second == function) return;
+			}
 
-		void Fire();
-		void Fire(void *data);
+			// Add the subscriber to the list.
+			SubscriberList.push_back(std::pair<EventSubscriber*, EventFunction>(subscriber, function));
+		}
+
+		void Unsubscribe(EventSubscriber *subscriber, EventFunction function)
+		{
+			// Search for subscriber and function in list.
+			size_t subscribers = SubscriberList.size();
+			for (size_t i = 0; i < subscribers; i++)
+			{
+				// Found it.
+				if (SubscriberList[i].first == subscriber && SubscriberList[i].second == function)
+				{
+					// Remove subscriber from the list.
+					SubscriberList.erase(SubscriberList.begin() + i);
+					break;
+				}
+			}
+		}
+
+		// Fire event with no data appended to it.
+		void Fire()
+		{
+			// Get the number of subscribers.
+			size_t subscribers = SubscriberList.size();
+
+			// Call all functions subscribed to the event.
+			for (size_t i = 0; i < subscribers; i++) (SubscriberList[i].first->*SubscriberList[i].second)(nullptr);
+		}
+
+		// Fire event with data appended to it.
+		void Fire(void *data)
+		{
+			// Get the number of subscribers.
+			size_t subscribers = SubscriberList.size();
+
+			// Call all functions subscribed to the event.
+			for (size_t i = 0; i < subscribers; i++) (SubscriberList[i].first->*SubscriberList[i].second)(data);
+		}
+
+		// Do we need to do something here?
+		~Event() {}
 
 	private:
-		std::vector<EventListener *> Listeners;
-		std::vector<Function> Functions;
+		std::vector<std::pair<EventSubscriber *, EventFunction>> SubscriberList;
 	};
 }
